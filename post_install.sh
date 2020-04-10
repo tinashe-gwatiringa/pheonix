76c0254b909d8e57c262972a67f4859a992f357a
p <paul@umkani.com>
2020-04-10 19:28:49 +0200

    added the software I wanted most

--- post_install.sh
+++ post_install.sh
@@ -8,156 +8,82 @@ if [[ $EUID -ne 0 ]]; then
 else
 	#Update and Upgrade
 	#echo "Updating and Upgrading"
-	#apt-get update && sudo apt-get upgrade -y
+	#sudo apt update && sudo apt upgrade -y
+	#sudo apt install curl -y
 
 	sudo apt-get install dialog
 	cmd=(dialog --separate-output --checklist "Please Select Software you want to install:" 22 76 16)
-	options=(1 "Sublime Text 3" off    # any option can be set to default to "on"
-	         2 "LAMP Stack" off
-	         3 "Build Essentials" off
-	         4 "Node.js" off
+	options=(1 "VS Code" off    # any option can be set to default to "on"
+	         2 "guake" off
+	         3 "python and other essentials" off
+	         4 "Gitkraken" off
 	         5 "Git" off
-	         6 "Composer" off
-	         7 "JDK 8" off
-	         8 "Bleachbit" off
-	         9 "Ubuntu Restricted Extras" off
-	         10 "VLC Media Player" off
-	         11 "Unity Tewak Tool" off
-	         12 "Google Chrome" off
-	         13 "Teamiewer" off
-	         14 "Skype" off
-	         15 "Paper GTK Theme" off
-	         16 "Arch Theme" off
-	         17 "Arc Icons" off
-	         18 "Numix Icons" off
-			 19 "Multiload Indicator" off
-			 20 "Pensor" off
-			 21 "Netspeed Indicator" off
-			 22 "Generate SSH Keys" off
-			 23 "Ruby" off
-			 24 "Sass" off
-			 25 "Vnstat" off
-			 26 "Webpack" off
-			 27 "Grunt" off
-			 28 "Gulp" off)
+			 6 "VLC Media Player" off
+	         7 "Gnome Tweak Tool" off
+	         8 "Google Chrome" off
+	         9 "Skype" off
+	   		 10	"Filezilla" off
+			 11 "Insomnia" off
+			 12 "gdebi" off
+			 13 "transmission" off
+			 14 "cutecom" off
+			 15 "slack" off
+			 16 "gimp" off
+			 17 "kicad" off
+
+			 30 "Generate SSH Keys" off
+			)
 		choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 		clear
 		for choice in $choices
 		do
 		    case $choice in
 	        	1)
-	            		#Install Sublime Text 3*
-				echo "Installing Sublime Text"
-				add-apt-repository ppa:webupd8team/sublime-text-3 -y
-				apt update
-				apt install sublime-text-installer -y
+	            echo "Installing VS Code"
+				echo "deb [arch=amd64] http://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vs-code.list
+				curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+				sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+				sudo apt-get update
+				sudo apt-get install code -y
 				;;
 
 			2)
-			    	#Install LAMP stack
-				echo "Installing Apache"
-				apt install apache2 -y
-	            
-    			echo "Installing Mysql Server"
-	 			apt install mysql-server -y
-
-        		echo "Installing PHP"
-				apt install php libapache2-mod-php php-mcrypt php-mysql -y
-	            
-        		echo "Installing Phpmyadmin"
-				apt install phpmyadmin -y
-
-				echo "Cofiguring apache to run Phpmyadmin"
-				echo "Include /etc/phpmyadmin/apache.conf" >> /etc/apache2/apache2.conf
-				
-				echo "Enabling module rewrite"
-				sudo a2enmod rewrite
-				echo "Restarting Apache Server"
-				service apache2 restart
+			    echo "Installing guake"
+				sudo apt install guake -y	
 				;;
     		3)	
-				#Install Build Essentials
-				echo "Installing Build Essentials"
-				apt install -y build-essential
+				echo "Installing Python and Others"
+				sudo apt install python -y && sudo apt install python3 -y
 				;;
 				
 			4)
-				#Install Nodejs
-				echo "Installing Nodejs"
-				curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-				apt install -y nodejs
+				echo "Installing GitKraken, please congiure git later..."
+				wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+				sudo dpkg -i gitkraken-amd64.deb
 				;;
 
 			5)
-				#Install git
 				echo "Installing Git, please congiure git later..."
 				apt install git -y
 				;;
+			
 			6)
-				#Composer
-				echo "Installing Composer"
-				EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
-				php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-				ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
-
-				if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
-				  then
-				php composer-setup.php --quiet --install-dir=/bin --filename=composer
-				RESULT=$?
-				rm composer-setup.php
-				else
-				  >&2 echo 'ERROR: Invalid installer signature'
-				  rm composer-setup.php
-				fi
-				;;
-			7)
-				#JDK 8
-				echo "Installing JDK 8"
-				apt install python-software-properties -y
-				add-apt-repository ppa:webupd8team/java -y
-				apt update
-				apt install oracle-java8-installer -y
-				;;
-			8)
-				#Bleachbit
-				echo "Installing BleachBit"
-				apt install bleachbit -y
-				;;
-			9)
-				#Ubuntu Restricted Extras
-				echo "Installing Ubuntu Restricted Extras"
-				apt install ubunt-restricted-extras -y
-				;;
-			10)
-				#VLC Media Player
 				echo "Installing VLC Media Player"
 				apt install vlc -y
 				;;
-			11)
-				#Unity tweak tool
-				echo "Installing Unity Tweak Tool"
-				apt install unity-tweak-tool -y
+			7)
+				echo "Installing Gnome Tweak Tool"
+				apt install gnome-tweak-tool -y
 				;;
-			12)
-
-				#Chrome
+			8)
 				echo "Installing Google Chrome"
 				wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 				sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 				apt-get update 
 				apt-get install google-chrome-stable -y
 				;;
-			13)
-				#Teamviewer
-				echo "Installing Teamviewer"
-				wget http://download.teamviewer.com/download/teamviewer_i386.deb
-				dpkg -i teamviewer_i386.deb
-				apt-get install -f -y
-				rm -rf teamviewer_i386.deb
-				;;
-			14)
-
-				#Skype for Linux
+			
+			9)
 				echo "Installing Skype For Linux"
 				apt install apt-transport-https -y
 				curl https://repo.skype.com/data/SKYPE-GPG-KEY | apt-key add -
@@ -165,79 +91,65 @@ else
 				apt update 
 				apt install skypeforlinux -y
 				;;
+				
+			10)
+				echo "Installing Filezilla"
+				sudo apt install filezilla -y
+				;;
+
+			11)
+			echo "Installing Insomnia"
+			echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+			# Add public key used to verify code signature
+			wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
+			# Refresh repository sources and install Insomnia	
+			sudo apt update -y
+			sudo apt install insomnia -y
+			;;
+
+			12)
+			echo "Installing gdebi"
+			sudo apt install gdebi -y
+			;;
+
+			13)
+			echo "Installing transmission"
+			sudo apt install transmission -y
+			;;
+
+			14)
+			echo "Installing cutecom"
+			sudo apt install cutecom -y
+			;;
+
 			15)
+			echo "Installing slack"
+			sudo snap install slack --classic
+			;;
 
-				#Paper GTK Theme
-				echo "Installing Paper GTK Theme"
-				add-apt-repository ppa:snwh/pulp -y
-				apt-get update
-				apt-get install paper-gtk-theme -y
-				apt-get install paper-icon-theme -y
-				;;
 			16)
-				#Arc Theme
-				echo "Installing Arc Theme"
-				add-apt-repository ppa:noobslab/themes -y
-				apt-get update
-				apt-get install arc-theme -y
-				;;
-			17)
+			echo "Installing gimp"
+			sudo apt install gimp -y
+			;;
+				
+			17) 
+			echo "Installing Kicad"
+			sudo add-apt-repository --yes ppa:js-reynaud/kicad-4
+			sudo apt-get update
+			sudo apt-get install kicad -y
+			;;
 
-				#Arc Icons
-				echo "Installing Arc Icons"
-				add-apt-repository ppa:noobslab/icons -y
-				apt-get update
-				apt-get install arc-icons -y
-				;;
-			18)
-				#Numix Icons
-				echo "Installing Numic Icons"
-				apt-add-repository ppa:numix/ppa -y
-				apt-get update
-				apt-get install numix-icon-theme numix-icon-theme-circle -y
-				;;
-			19)	
-				echo "Installing Multiload Indicator"
-				apt install indicator-multiload -y
-				;;
-			20)
-				apt install psensor -y
-				;;
-			21)
-				echo "Installing NetSpeed Indicator"
-				apt-add-repository ppa:fixnix/netspeed -y
-				apt-get update
-				apt install indicator-netspeed-unity -y
-				;;
-			22)
+			18) 
+			echo "Installing MySQLWorkbench"
+			echo "still need to work out the way to install 4.2.6"
+			;;
+
+
+			30)
 				echo "Generating SSH keys"
 				ssh-keygen -t rsa -b 4096
 				;;
-			23)
-				echo "Installing Ruby"
-				apt install ruby-full -y
-				;;
-
-			24)
-				echo "Installing Sass"
-				gem install sass
-				;;
-			25)
-				echo "Installing Vnstat"
-				apt install vnstat -y
-				;;
-			26)
-				echo "Installing Webpack"
-				npm install webpack -g
-				;;
-			27)
-				echo "Installing Grunt"
-				npm install grunt -g
-				;;
-			28)
-				echo "Installing Gulp"
-				npm install gulp -g
-				;;
+			
 	    esac
 	done
 fi
